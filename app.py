0879ae7920b1f875f9685a2054106f6e2efd3ab3
paolo.bevilacqua <paolo.bevilacqua@soft.it>
2023-09-18 22:43:55 +0200

    updated

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-
 """
 Routes and views for the flask application.
 """
 
-from flask import Flask
-
-import boto3, botocore
-import pandas as pd
 import json
+
+import boto3
+import pandas as pd
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/')
+def default():
+    return 'helloo'
+
+
 @app.route('/home')
 def home(id):
     """Renders the home page."""
@@ -29,25 +32,24 @@ def home(id):
 
     join_df = df_history.merge(df_data, left_on='songId', right_on='id', how='inner')
     filtered_df = join_df
-    if(id!=None):
-        filtered_df = join_df.where(join_df["userId"]==id)
-    final_df = filtered_df.rename(columns={'name': 'Title', 'artists': 'Artist(s)'})[['Date', 'Text', 'Title', 'Artist(s)']]
+    if (id != None):
+        filtered_df = join_df.where(join_df["userId"] == id)
+    final_df = filtered_df.rename(columns={'name': 'Title', 'artists': 'Artist(s)'})[
+        ['Date', 'Text', 'Title', 'Artist(s)']]
 
     return json(final_df)
 
 
 def download_from_s3(s3, bucket_name, object_name):
-    
     try:
 
         # Retrieve the contents of the S3 object
         response = s3.get_object(Bucket=bucket_name, Key=object_name)
-    
-        return pd.read_csv(response.get('Body')) # 'Body' is a key word
 
-    except Exception as e:  
+        return pd.read_csv(response.get('Body'))  # 'Body' is a key word
+
+    except Exception as e:
         print('ERROR: ', e)
-
 
 
 if __name__ == '__main__':
